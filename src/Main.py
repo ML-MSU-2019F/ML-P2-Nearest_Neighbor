e17eda2a85f5b123139b297d06583e83740e3d97
Keefer Sands <keefer212@gmail.com>
2019-10-07 12:05:33 -0600

    Think I fixed the pycharm import issue

--- src/Main.py
+++ src/Main.py
@@ -5,7 +5,7 @@ abalone = DataSet("../data/abalone.data")
 #create a random map to pick from
 
 abalone.makeRandomMap(x_fold_validation)
-for i in range(0,x_fold_validation):
+for i in range(0, x_fold_validation):
     one = abalone.getRandomMap(i)
     rest = abalone.getAllRandomExcept(i)
     #here we would train using rest, then classify/regress one
